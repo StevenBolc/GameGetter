dd2de3f0adcafb5d918f1d0113bc46113c45d5e7
Kris Marshall <rflctnofu@hotmail.com>
2022-08-04 13:09:02 -0400

    got user creation route functional

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
