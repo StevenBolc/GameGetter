98fe1b4bff9df2e09b0980dc6c87d2aca3775146
Kris Marshall <rflctnofu@hotmail.com>
2022-08-04 10:59:03 -0400

    router files made

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./userRoutes');
+const userRoutes = require('./userRoutes');
 // const postRoutes = require('./postRoutes');
 // const commentRoutes = require('./commentRoutes');
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 // router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api');
 
-router.use('/', homeRoutes);
+router.use('/api', apiRoutes),
+    router.use('/', homeRoutes);
 
 module.exports = router;
