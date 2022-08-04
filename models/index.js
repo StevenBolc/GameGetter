5668ddb640aaafe50f25fda907e401bd3cca872f
StevenBolc <steven.bolcar95@gmail.com>
2022-08-04 11:24:58 -0400

    added user.js/added meat to index.js

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Videogame = require('./Videogame');
+const User = require('./User');
+
+Videogame.belongsTo(User, {
+  foreignKey: 'videogame_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Videogame, {
+  foreignKey: 'videogame_id'
+});
+
+module.exports = { Videogame, User };
