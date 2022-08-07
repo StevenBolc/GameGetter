7b1f5cdc23ebd3a2864bb064bbff959fac3ba5ff
TDEvans97 <tylerdevansmusic@gmail.com>
2022-08-07 12:00:53 -0400

    Began receiving responses from the api

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,11 @@ const withAuth = require('../utils/auth');
 const asyncHandler = require('express-async-handler');
 const toSlug = require("../utils/helpers");
 const { Op } = require("sequelize");
+const { response } = require('express');
+const sequelize = require('../config/connection');
+const { response, request } = require('express');
+const axios = require('axios').default;
+require('dotenv').config();
 
 router.get('/', asyncHandler(async (req, res) => {
     // res.json({ message: 'get request successful' })
@@ -51,8 +56,8 @@ router.get('/dashboard', withAuth, asyncHandler(async (req, res) => {
 }));
 
 router.post('/combine', asyncHandler(async (req, res) => {
-    const videogames = await Videogame.findAll();  //used for data entry ---> { offset: 13612, limit: 5000 }
-    const slugs = videogames.map(game => {   
+    const videogames = await Videogame.findAll();  //used for data entry ---> { offset: int, limit: int }
+    const slugs = videogames.map(game => {
         const gamename = game.name;
         return {
             name: gamename,
@@ -78,5 +83,13 @@ router.post('/combine', asyncHandler(async (req, res) => {
     res.json({ message: 'post request successful' });
 }));
 
+// SELECT slug FROM videogames 
+// store that in a variable
+// for each slug in the variable, pass one to the ${slug}
+
+axios
+    .get(`https://api.rawg.io/api/games/super-mario-bros?key=7ee3b87431ea4765a625d53ee46cc6c2&dates=2019-09-01,2019-09-30&platforms=18,1,7`)
+    .then((response) => console.log(response.data.description, response.data.background_image, response.data.website))
+    .catch((error) => console.log(error));
 
 module.exports = router;
